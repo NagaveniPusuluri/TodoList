81de6411a7e38d9906cae03f8842c920e78c74d4
NagaveniPusuluri <93608500+NagaveniPusuluri@users.noreply.github.com>
2024-12-14 12:37:33 +0530

    Update script.js
    updated functionality

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const form = document.querySelector('.form');
 const text = document.querySelector(".input-name");
 const add = document.querySelector(".add");
+const ul=document.querySelector(".ul");
 
 form.addEventListener("submit",function (e) {
     e.preventDefault();
+    //creating list tag
     const list = document.createElement("li");
     list.setAttribute("class","list")
-    list.textContent = text.value;
+    const p = document.createElement("p");
+    p.textContent =text.value;
+    list.appendChild(p);
     document.querySelector(".ul").appendChild(list);
+    //creating button
+    const removeBtn=document.createElement("button");
+    removeBtn.innerText="Remove";
+    list.appendChild(removeBtn);
     text.value = "";
 });
 
-const ul =document.querySelector('.ul');
-ul.addEventListener("click", function(e) {  
-    let list = document.querySelector(".list");  
-  //  list.textContent =''; 
-    ul.removeChild(list);
-}); 
+ul.addEventListener("click",function(e){
+  e.preventDefault();
+  if(e.target.innerHTML==="Remove")
+  ul.removeChild(e.target.parentElement);
+})
